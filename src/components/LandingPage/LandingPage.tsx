66ca19af63f59f3305af2d3e4661c1ee566541e7
Fedeaguilera94 <aguilerafederico94@gmail.com>
2021-10-01 01:40:12 -0300

    landing

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,5 +1,40 @@
 import React from "react";
+import styles from "./LandingPage.module.scss";
+import { Link } from "react-router-dom";
 
 export default function LandinPage() {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.btngrid}>
+        <div className={styles.btnc}>
+          <Link to="/register">
+            <button className={styles.btn}>Registrese</button>
+          </Link>
+        </div>
+      </div>
+      <div className={styles.containerTitle}>
+        <p className={styles.title}>EL POTRERO</p>
+      </div>
+      <div className={styles.backgroundInfo}>
+        <div className={styles.busca}>
+          <p className={styles.infotitle}>Busca</p>
+          <p className={styles.parrafo}>
+            Conocé la disponibilidad de tus canchas preferidas
+          </p>
+        </div>
+        <div className={styles.reserva}>
+          <p className={styles.infotitle}>Reservá</p>
+          <p className={styles.parrafo}>
+            Tenés toda la información para hacer tu reserva instantánea
+          </p>
+        </div>
+        <div className={styles.alquila}>
+          <p className={styles.infotitle}>Jugá</p>
+          <p className={styles.parrafo}>
+            Vos preocupate por romperla en el partido
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
