2de8c6c395013dd2ef3c2a4aa7ad55f2a853167b
Rodrii-github <rodrigo93batista@gmail.com>
2021-10-11 02:42:41 -0300

    success and failed

--- src/components/Failed/Failed.tsx
+++ src/components/Failed/Failed.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Failed.module.scss";
 
 export default function Failed() {
   return (
-    <div>
-      <h1>Pago rechazado</h1>
+    <div className={styles.container}>
+      <div>
+        <h1>Tu pago fue rechazado</h1>
+      </div>
+      <span>¿Qué pasó?</span>
+      <p>
+        No pudimos avanzar con el pago, si quieres puedes intentar con otro
+        medio de pago
+      </p>
+      <div>
+        <Link to="/home">
+          <button>Volver a Home</button>
+        </Link>
+      </div>
     </div>
   );
 }
--- src/components/Success/Success.tsx
+++ src/components/Success/Success.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Success.module.scss";
 
 export default function Success() {
   return (
-    <div>
-      <h1>Pago Aceptado</h1>
+    <div className={styles.container}>
+      <h1>¡Listo! Se acredito tu pago</h1>
+      <Link to="/home">
+      <button>Volver a Home</button>
+      </Link>
     </div>
   );
 }
