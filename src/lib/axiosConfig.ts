ba0a4f8ebe6f0ff7ba2845c57df67fda88e15fcc
sgonzalezlonzieme <sgonzalezlonzieme@gmail.com>
2021-10-12 10:53:22 -0300

    sprint2 10:53

--- src/lib/axiosConfig.ts
+++ src/lib/axiosConfig.ts
@@ -6,7 +6,7 @@ const token = window.sessionStorage.getItem("jwt") || ""
 
 console.log(token, "ESTAMOS EN EL ARCHIVO DE CONFIG AXIOS")
 
-instance.defaults.headers.common['Authorization'] = token;
+instance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 instance.defaults.headers.post["Content-Type"] = "application/json";
 
 export default instance
