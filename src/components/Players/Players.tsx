e34ed1c963ac60fb16cc256247bdd2f5aeb7d8ca
jesusmirena <jesusamh16@gmail.com>
2021-10-14 21:41:36 -0500

    Componente Players listo

--- src/components/Players/Players.tsx
+++ src/components/Players/Players.tsx
@@ -1,7 +1,69 @@
-import React from "react";
-
+import axios from "../../lib/axiosConfig";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import styles from "./Players.module.scss";
 function Players() {
-  return <div></div>;
+  const [jugadores, setJugadores]: any = useState([]);
+
+  const players = async () => {
+    const data = await axios.get("http://localhost:3001/player");
+    var res = await data.data;
+    setJugadores(res);
+  };
+  useEffect(() => {
+    players();
+  }, []);
+  return (
+    <div>
+      <h1 className={styles.title}>Jugadores</h1>
+      <table className={styles.jugadores}>
+        <thead>
+          <tr>
+            <th>Imagen</th>
+            <th>Nombre</th>
+            <th>Sexo</th>
+            <th>Posicion</th>
+            <th>Calificacion</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {jugadores.map((p: any) => {
+            return (
+              <tr>
+                <td>
+                  <Link to="/jugador">
+                    <img className={styles.imagenTabla} src={p.image} alt="" />
+                  </Link>
+                </td>
+                <td>
+                  <Link to="/jugador">{p.name}</Link>
+                </td>
+                <td>{p.gender}</td>
+                <td>{p.position}</td>
+                <td>{p.qualification} / 5</td>
+                <td>
+                  <form>
+                    <input
+                      className={`${styles.boton} ${styles.botonInvitar}`}
+                      type="submit"
+                      name=""
+                      value="Invitar"
+                    />
+                    <input
+                      className={`${styles.boton} ${styles.botonCalificar}`}
+                      type="submit"
+                      value="Calificar"
+                    />
+                  </form>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Players;
